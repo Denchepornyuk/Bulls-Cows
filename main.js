c34d31d11dc4e15d398493dae47f9a95c50b24a2
Denys <denys.cheporniuk@gmail.com>
2019-11-12 21:33:40 +0200

    fix game

--- main.js
+++ main.js
@@ -1,22 +1,33 @@
 const gameButton = document.getElementById('button'),
       gameInput = document.getElementById('player__number'),
       countResult = document.getElementById('result'),
-      info = document.getElementById('info');
+      info = document.getElementById('info'),
+      label = document.getElementById('game__label'),
+      game = document.getElementById('game'),
+      generatedNumber = Math.floor(Math.random() * 10000).toString();
+
 
 gameButton.addEventListener('click', () => {
-  const generatedNumber = Math.floor(Math.random() * 10000) + '';
   const enteredNumber = gameInput.value;
 
   const gameResult = bullsAndCows(generatedNumber, enteredNumber);
-  showResult(generatedNumber, enteredNumber, gameResult);
+  showResult(gameResult);
+  showComputerNumber(generatedNumber, enteredNumber);
+
+  if (generatedNumber === enteredNumber) {
+    game.classList.add('winner');
+  }
 });
 
 function bullsAndCows(generatedNumber, enteredNumber) {
   const countBullsAndCows = { 'bulls': 0, 'cows': 0 };
   if ([...new Set(enteredNumber)].length !== 4) {
-    return undefined;
+    showError();
+    return '';
+  } else {
+    hideError();
   }
-  console.log(generatedNumber, enteredNumber)
+
   for (let i = 0; i < 4; i++) {
     if (enteredNumber[i] === generatedNumber[i]) {
       countBullsAndCows['bulls']++;
@@ -27,9 +38,23 @@ function bullsAndCows(generatedNumber, enteredNumber) {
   return countBullsAndCows;
 }
 
-function showResult(generatedNumber, enteredNumber, gameResult) {
+function showResult(gameResult) {
   countResult.innerHTML = `<div class='game__bulls'> Bulls: ${gameResult['bulls']}</div>\
                            <div class='game__cows'>Cows: ${gameResult['cows']}</div>`;
-  
-  info.innerHTML = `<span class="game__computer">Computer number: ${generatedNumber}</span>`;
+}
+
+function showComputerNumber(generatedNumber, enteredNumber) {
+  let number = generatedNumber.split('').map((item, index) => {
+    return item === enteredNumber[index] ? item : '*';
+  });
+
+  info.innerHTML = `<span class="game__computer">Computer number: ${number.join('')}</span>`;
+}
+
+function showError() {
+  label.classList.add('error'); 
+}
+
+function hideError() {
+  label.classList.remove('error'); 
 }
